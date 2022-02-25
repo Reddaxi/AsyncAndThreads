bf768b327f5fafe938cf472ee67fce09020df3fb
Mathias Blaaberg <blaaber@live.dk>
2022-02-25 09:11:11 +0100

    updates

--- AsyncAndThreads/Program.cs
+++ AsyncAndThreads/Program.cs
@@ -1,35 +1,53 @@
 ﻿//MS Breakfast analogy
 //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
-main().Wait();
 
+
+//Async: You go to the store and buy pasta. Let me know when you get back, to make dinner together. Meanwhile, I’ll prepare sauce and drinks.
+
+//Threads: You boil the water. I’ll heat the tomato sauce. While the water is boiling, ask me and I’ll put the pasta in. When the sauce is hot, you can add cheese.
+//When both are done, I’ll sit down and you serve dinner. Then we eat.
+
+//Multithreading is about workers, Asynchronous is about tasks
+//https://www.baeldung.com/cs/async-vs-multi-threading
+
+
+//Noter:
+
+main().Wait();
+// Async er "bare" et flag til compileren om at der bruges "await" i denne metode.
+// Uden "await", gør "async" ikke noget.
 async Task main()
 {
 
-    Task<string> textTask = SavePictureAsync();
     Task saveUserTeamRelationsTask;
     Task saveUserTask;
+    Task saveUserPictureTask;
     //using (DbContext context = new DbContext())
     {
-        saveUserTeamRelationsTask = SaveTeamRelationsAsync();
-        saveUserTask = SaveUserAsync();
+        saveUserTeamRelationsTask = SaveTeamRelationsAsync(); //4 sekunder
+        saveUserTask = SaveUserAsync();                       //2 sekunder
+        saveUserPictureTask = SavePictureAsync();             //5 sekunder
     }
+    await saveUserTask;
+    SomeTaskStarter(); //Do when save user is done, but you're still waiting for the other 2 tasks.
 
-    SomeTaskStarter();
-
-    await Task.WhenAll(textTask, saveUserTeamRelationsTask, saveUserTask);
+    await Task.WhenAll(saveUserPictureTask, saveUserTeamRelationsTask);
     WriteMessageWithColour("All tasks have finished!", ConsoleColor.Cyan);
     await SendUserCreatedNotificationAsync();
 }
 
-async Task SomeTaskStarter()
+void SomeTaskStarter()
 {
-    await Task.Run(() => {
-        Thread.Sleep(5000);
+    WriteMessageWithColour("I'm spinning up another thread!", ConsoleColor.Magenta);
+    Task.Run(() =>
+    {
         WriteMessageWithColour("I'm written in another thread!", ConsoleColor.Magenta);
+        Thread.Sleep(5000);
+        WriteMessageWithColour("I'm also written in another thread!", ConsoleColor.Magenta);
     });
 }
 
-async Task<string> SavePictureAsync()
+async Task SavePictureAsync()
 {
     WriteMessageWithColour("Saving the picture...", ConsoleColor.White);
     WriteMessageWithColour("Spinning up really old hard drive...", ConsoleColor.White);
@@ -37,9 +55,9 @@ async Task<string> SavePictureAsync()
     WriteMessageWithColour("Found file location...", ConsoleColor.White);
     await Task.Delay(1000);
     WriteMessageWithColour("Writing to drive...", ConsoleColor.White);
-    await Task.Delay(1000);
+    await Task.Delay(2000);
     WriteMessageWithColour("Succesfully saved the picture!", ConsoleColor.White);
-    return "/some/path/to/picture.png";
+    return;
 }
 
 async Task SaveTeamRelationsAsync()
@@ -75,6 +93,6 @@ async Task SendUserCreatedNotificationAsync()
 void WriteMessageWithColour(string message, ConsoleColor color)
 {
     Console.ForegroundColor = color;
-    Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " - "  + message);
+    Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " - " + message);
     Console.ForegroundColor = ConsoleColor.White;
 }
